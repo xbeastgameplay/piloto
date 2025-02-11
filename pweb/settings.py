5c027ef76b1b92c0e98a3f062074d12d950203e4
xbeastgameplay <160731032+xbeastgameplay@users.noreply.github.com>
2025-02-11 19:46:51 -0300

    Teste

--- pweb/settings.py
+++ pweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c(o4g(*cs=^t34p)&(p&y&%-@ir62u)hd&n1ckkj=!pb78t6h6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
